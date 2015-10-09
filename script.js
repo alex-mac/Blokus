e7769d86bed4e52e82232e79fefb0b8b27018109
alex-mac <meet.alexmac@gmail.com>
2015-10-09 14:30:47 -0700

    start point for Blokus, red gameboard

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
 	console.log("i'm ready");
 
-	$(document).keydown(function(e){
-		if (e.which == 39) { //right
-			$("#square").style.left="50px";
-			// $("#square").animate({'left': '+=100'});
-
-			// $('#bottom_arrow').animate({ 'left': '+=100' });
-		}
-	})
 });
