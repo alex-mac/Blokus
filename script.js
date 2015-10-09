f7a0c0c8bcbc09b351b1e711626e652f51161770
alex-mac <meet.alexmac@gmail.com>
2015-10-09 16:04:40 -0700

    finished the game-board git add .

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-$(document).ready(function(){
-	var gameBoard = [];
 
-
-	var setGameBoard = function(){
-		for (var i = 0; i < 20; i++){
-			for (var j = 0; j < 20; j++){
-				gameBoard[i][j] = null;
-			}
+var setGameBoard = function(){
+	for (var i = 0; i < 20; i++){
+		gameBoard[i] = []
+		for (var j = 0; j < 20; j++){
+			gameBoard[i][j] = null;
+			var gameTile = $("<div class='game-tile'></div>");
+			$("#game-board").append(gameTile);
 		}
-
 	}
+}
+
+var gameBoard = [];
+
+
+$(document).ready(function(){
+
+	setGameBoard();
+
+	
 
 console.log(gameBoard);
 //play a piece condition / function
