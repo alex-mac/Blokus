9e3a4a1c111fa861173943d2efc80fd105dc98bd
alex-mac <meet.alexmac@gmail.com>
2015-10-09 15:32:43 -0700

    centered the board

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-	console.log("i'm ready");
+	var gameBoard = [];
+
+
+	var setGameBoard = function(){
+		for (var i = 0; i < 20; i++){
+			for (var j = 0; j < 20; j++){
+				gameBoard[i][j] = null;
+			}
+		}
+
+	}
+
+console.log(gameBoard);
+//play a piece condition / function
 
 });
