35dccd0311a4a353b859a09cdc0c3f868bde2cca
alex-mac <meet.alexmac@gmail.com>
2015-10-10 00:39:18 -0700

    extended array, drew a board, working on the UI still

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 
 var setGameBoard = function(){
-	for (var i = 0; i < 20; i++){
+	
+	//sets up arrays, appends 22 "squares" to each row and column
+	for (var i = 0; i < 22; i++){
 		gameBoard[i] = []
-		for (var j = 0; j < 20; j++){
+		for (var j = 0; j < 22; j++){
 			gameBoard[i][j] = null;
-			var gameTile = $("<div class='game-tile'></div>");
-			$("#game-board").append(gameTile);
+			var gameTile = $("<div class='game-tile' id='square" + i + "-" + j + "'></div>");
+			$("#game-stats-container").append(gameTile);
+
+			if (i === 0 || i === 21 || j === 0 && j === 21) {
+				var targetSquareDiv = "#sqaure" + i + "-" + j;
+				$(targetSquareDiv).css("background-color, black");
+				console.log(targetSquareDiv);
+			}
 		}
 	}
+
 }
+
 
 var gameBoard = [];
 
