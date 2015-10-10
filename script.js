9d5e083481fc940082769e0182529038ddc3209b
alex-mac <meet.alexmac@gmail.com>
2015-10-10 00:49:39 -0700

    completed the initial state of the game (gameBoard is completed)

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ var setGameBoard = function(){
 			var gameTile = $("<div class='game-tile' id='square" + i + "-" + j + "'></div>");
 			$("#game-stats-container").append(gameTile);
 
-			if (i === 0 || i === 21 || j === 0 && j === 21) {
-				var targetSquareDiv = "#sqaure" + i + "-" + j;
-				$(targetSquareDiv).css("background-color, black");
+			// colors the unplayable arrays
+			if (i === 0 || i === 21 || j === 0 || j === 21) {
+				var targetSquareDiv = "#square" + i + "-" + j;
+				$(targetSquareDiv).css("background-color", "black");
+				$(targetSquareDiv).css("border","none");
 				console.log(targetSquareDiv);
 			}
 		}
 	}
 
+	// sets the values of the corners to make them playable
+	//need to append classes to them later to make them color specific
+	gameBoard[0][0] = 1;
+	gameBoard[0][21] = 1;
+	gameBoard[21][0] = 1;
+	gameBoard[21][21] = 1;
 }
 
 
